9223c9cdff3a0dc57be1505eb1a26767b970d51b
Artem_Wolf <artem.mizyuk@live.com>
2025-05-26 18:22:24 +0300

    Змінено тип стовпця password_hash з String(128) на Text у моделі User для підтримки довших паролів. Оновлено команду запуску веб-сервера у docker-compose.yml для спрощення конфігурації.

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ class Task(db.Model):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.Text, nullable=False)  # <-- було String(128), тепер Text
     role = db.Column(db.String(10), default='user')  # 'admin' або 'user'
     sessions = db.relationship('UserSession', backref='user', cascade="all, delete-orphan", lazy=True)
 
